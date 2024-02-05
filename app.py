8df9f0ee837628ae1b084794f5b6310a793ea9a2
sharma172 <104724760+sharma172@users.noreply.github.com>
2024-02-05 08:16:44 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template
+import numpy as np
+import pickle
+import sklearn
+print(sklearn.__version__)
+# loading models
+dtr = pickle.load(open('dtr.pkl', 'rb'))
+preprocessor = pickle.load(open('preprocessor.pkl', 'rb'))
+
+# flask app
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route("/predict", methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        year = request.form['Year']
+        average_rain_fall_mm_per_year = request.form['average_rain_fall_mm_per_year']
+        pesticides_tonnes = request.form['pesticides_tonnes']
+        avg_temp = request.form['avg_temp']
+        area = request.form['Area']
+        item = request.form['Item']
+
+        features = np.array(
+            [[year, average_rain_fall_mm_per_year, pesticides_tonnes, avg_temp, area, item]], dtype=object)
+        transformed_features = preprocessor.transform(features)
+        prediction = dtr.predict(transformed_features).reshape(1, -1)
+
+        return render_template('index.html', prediction=prediction)
+
+if __name__ == "__main__":
+    app.run(debug=True)
